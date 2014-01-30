1842f2e04d098aa161cd753b7dcdddccf2686776
Derek Petillo <derek.petillo@gmail.com>
2014-01-29 21:42:25 -0500

    updating test so it builds

--- Test/Program.cs
+++ Test/Program.cs
@@ -8,21 +8,21 @@ using Roslyn.Compilers.CSharp;
 
 namespace Genesis.Test
 {
-    using GenesisGenerator;
+   //using GenesisGenerator;
 
     class Program
     {
         static void Main(string[] args)
         {
-            GenesisGenerator.Generator g = new GenesisGenerator.Generator();
-            CompilationUnitSyntax cus = g.GetCompilationUnit();
-            SyntaxTree syntaxTree = SyntaxTree.Create(cus);
+      //      GenesisGenerator.Generator g = new GenesisGenerator.Generator();
+            //CompilationUnitSyntax cus = g.GetCompilationUnit();
+            //SyntaxTree syntaxTree = SyntaxTree.Create(cus);
 
-            using (var sw = new StreamWriter(File.Open("..\\..\\Output\\output.cs", FileMode.Create, FileAccess.Write)))
-            {
-                syntaxTree.GetRoot().NormalizeWhitespace().WriteTo(sw);
-                sw.Flush();
-            }
+            //using (var sw = new StreamWriter(File.Open("..\\..\\Output\\output.cs", FileMode.Create, FileAccess.Write)))
+            //{
+            //    syntaxTree.GetRoot().NormalizeWhitespace().WriteTo(sw);
+            //    sw.Flush();
+            //}
         }
     }
 }
